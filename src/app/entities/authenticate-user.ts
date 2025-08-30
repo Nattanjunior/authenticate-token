f86881c7df62db85c1539c219cdc05d06db4ddab
nattanjunior <fnatanieljunior@gmail.com>
2025-08-30 01:13:30 -0300

    feat: add adds folder structures, using good practices, user credential validation file created

---
+++ src/app/entities/authenticate-user.ts
@@ -0,0 +1,27 @@
+type Credentials = {
+  username: string;
+  password: string;
+};
+
+type User = {
+  userId: string;
+};
+export class AuthenticationService {
+  public authenticate(credentials: Credentials): Promise<User> {
+    const delay = Math.random() * 5000;
+    const ValidationPasswordUser = credentials.password.toUpperCase() == credentials.username;
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (ValidationPasswordUser) {
+          resolve({
+            userId: credentials.username,
+          });
+        } else {
+          reject(new Error('Invalid credentials'));
+        }
+      }, delay);
+    });
+  }
+
+}
